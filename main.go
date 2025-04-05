7ebb2e1044ef54d173ea2875691020eab9c3c9c8
morimorig3 <mori.nto@outlook.jp>
2025-04-05 10:44:51 +0900

    feat: routerへtaskController追加

--- main.go
+++ main.go
@@ -10,9 +10,16 @@ import (
 
 func main() {
 	db := db.NewDB()
+
 	userRepository := repository.NewUserRepository(db)
+	taskRepository := repository.NewTaskRepository(db)
+
 	userUseCase := usecase.NewUserUseCase(userRepository)
+	taskUseCase := usecase.NewTaskUseCase(taskRepository)
+
 	userController := controller.NewUserController(userUseCase)
-	e := router.NewRouter(userController)
+	taskController := controller.NewTaskController(taskUseCase)
+
+	e := router.NewRouter(userController, taskController)
 	e.Logger.Fatal(e.Start(":8080"))
 }
